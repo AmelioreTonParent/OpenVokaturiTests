4aa47c4fb67a62a2f88444fc2708e3124303a8bf
Gabriel Barbier <amelioretonparent@gmail.com>
2020-02-19 23:43:43 +0100

    Add audio data for accuracy inputs. And results !
    Not a very good engine finally, with a detection of anger around 40% only.

---
+++ cmake/FindOpenVokaturi.cmake
@@ -0,0 +1,42 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+#[=======================================================================[.rst:
+FindOpenVokaturi
+-------
+
+Finds the OpenVokaturi library.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module provides the following imported targets, if found:
+
+``OpenVokaturi::OpenVokaturi``
+  The OpenVokaturi library
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``OpenVokaturi_FOUND``
+  True if the system has the OpenVokaturi library.
+``OpenVokaturi_VERSION``
+  The version of the OpenVokaturi library which was found.
+``OpenVokaturi_INCLUDE_DIRS``
+  Include directories needed to use OpenVokaturi.
+``OpenVokaturi_LIBRARIES``
+  Libraries needed to link to OpenVokaturi.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``OpenVokaturi_INCLUDE_DIR``
+  The directory containing ``OpenVokaturi.h``.
+``OpenVokaturi_LIBRARY``
+  The path to the OpenVokaturi library.
+
+#]=======================================================================]
